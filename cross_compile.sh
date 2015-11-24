6859223d1cb36842ce16784fc456af5080d3d92f
ChanMin Kim <kcm1700@gmail.com>
2015-11-25 05:55:46 +0900

    cross_compile.sh: build 끝나고 ath9k driver 복사하는 스크립트 호출

--- cross_compile.sh
+++ cross_compile.sh
@@ -13,4 +13,5 @@ mkdir -p build_output
 export PATH=$TOOLCHAIN_DIR/bin:$PATH
 export STAGING_DIR=$TOOLCHAIN_DIR
 make ${MK} O=build_output ARCH=mips CROSS_COMPILE=mips-openwrt-linux- menuconfig
-make ${MK} O=build_output ARCH=mips CROSS_COMPILE=mips-openwrt-linux- "$@"
+make ${MK} O=build_output ARCH=mips CROSS_COMPILE=mips-openwrt-linux- "$@" \
+&& ./cp_ath9k_drivers.sh
